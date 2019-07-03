4a8d7cf073a3c656d1e180e37161fd1317a08dda
Preston Elliott <preston.elliott24@gmail.com>
2019-07-03 09:33:16 -0400

    small updates

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -5,7 +5,8 @@ const Stock = (props) => (
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{props.stock.name}</h5>
-        <p className="card-text">{props.stock.ticker} - {props.stock.price}</p>
+        <h7 className="card-title">{props.stock.type}</h7><br></br>
+        <p className="card-text">{props.stock.ticker} - ${props.stock.price}</p>
         {props.buyStock ? <button onClick={() => {props.buyStock(props.stock)}}> Buy Stock</button> : <button onClick={() => props.sellStock(props.stock)}> Sell Stock</button>} 
       </div>
     </div>
